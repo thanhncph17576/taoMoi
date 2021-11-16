b043c0cfdead88e0a55e4746ebc78d6f0f99e4a6
thanhncph17576 <thanhncph17576@fpt.edu.vn>
2021-11-16 23:01:23 +0700

    thanhDao

--- src/thanhnc/Thanhnc.java
+++ src/thanhnc/Thanhnc.java
@@ -17,6 +17,7 @@ public class Thanhnc {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("CCCCCC");
+        System.out.println("thanhnc.Thanhnc.main()");
     }
     
 }
